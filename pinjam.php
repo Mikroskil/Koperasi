43f37f8fa046f6b924d474b62a549777b2ee0f56
Ricky7 <black.id64@gmail.com>
2013-11-13 14:25:50 +0700

    Create pinjam.php
    form pengajuan pinjaman

---
+++ pinjam.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Merpati Pos</title>
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">	
+	<link rel="stylesheet" href="fontawsome/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/a.css">
+		
+	<SCRIPT TYPE="text/javascript" src="js/jquery.js"></SCRIPT>
+ 
+ 	
+</head>
+<body>
+
+ 
+		<div class="sidebar">
+			<div class="heading1">
+			<img src="img/kope.png" class="logo">
+			<h1>Merpati Pos</h1>
+			<div class="grs"></div>
+			</div>
+			<nav>
+			<ul class="icons-ul">
+				<li><a href="#"><i class="icon-plus-sign icon-li"></i>AJUKAN PINJAMAN</a><hr></li>
+				<li><a href="daftar_transaksi.php"><i class="icon-user icon-li"></i>LIHAT DAFTAR TRANSAKSI</a><hr></li>
+				<li><a href="index.php"><i class="icon-off icon-li"></i>KELUAR</a><hr></li>
+			</ul>
+			</nav>
+		</div>
+		
+		
+		<div class="container">
+			
+				<div class="row">
+					
+						<div class="pull-right">
+							<h5>Berhasil Masuk <i class="icon-thumbs-up"></i></h5>
+						</div>
+						
+								 
+
+				</div>
+				
+				<div class="row">
+					
+						<div class="col-md-2  "> </div>
+						<div class="col-md-10  page_header">
+								<h3><font color="#3C2A2A"><center>Ajukan Pinjaman</center></font></h3>
+								<hr>
+						</div>				 
+
+				</div>
+				
+				<div class="row">
+					<div class="col-md-2"></div>
+					<div class="col-md-10">
+					
+						<font face="verdana,arial" size=-1>
+						<center>
+												<form method="post" action="#" name=pform>
+														<table class="table table-responsive table-key-value pull-center">
+														<tbody>
+															<tr><td><font face="verdana,arial" size=-1><center>NIK</center> </font></td><td>: <input type="text" name="nik" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Nama</center> </font></td><td>: <input type="text" name="name" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Alamat</center> </font></td><td>: <textarea cols="18" name="ad"></textarea></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Email</center></font></td><td>: <input type="text" name="email" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>No. Telp</center> </font></td><td>: <input type="text" name="cp" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Nominal Rp.</center> </font></td><td>: <input type="text" name="nom" /></td></tr>
+															<tr><td></td><td><button type="submit" class="btn"><center>Ajukan</center></button></td></tr>
+															</font>
+														</tbody>
+														</table>
+												</form>
+						</center>
+					</div>
+				</div>
+		</div>
+
+
+		
+
+
+ 
+	
+</body>
+</html>
