e8247c4de09a9db10b2679e0f2c866c2084c5ae6
Ricky7 <black.id64@gmail.com>
2013-11-13 14:25:07 +0700

    Create member_list.php
    daftar anggota

---
+++ member_list.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Daftar Anggota</title>
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">	
+	<link rel="stylesheet" href="fontawsome/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/a.css">
+		
+	<SCRIPT TYPE="text/javascript" src="js/jquery.js"></SCRIPT>
+ 
+ 	
+</head>
+<body>
+
+ 
+		<div class="sidebar">
+			<div class="heading1">
+			<img src="img/kope.png" class="logo">
+			<h1>Admin</h1>
+			<div class="grs"></div>
+			</div>
+			<nav>
+			<ul class="icons-ul">
+				<li><a href="add_admin.php"><i class="icon-plus-sign icon-li"></i>Tambah Admin</a><hr></li>
+				<li><a href="add_member.php"><i class="icon-plus-sign icon-li"></i>Tambah Anggota</a><hr></li>
+				<li><a href="#"><i class="icon-book icon-li"></i>Daftar Anggota</a><hr></li>
+				<li><a href="#"><i class="icon-off icon-li"></i>Keluar</a><hr></li>
+			</ul>
+			</nav>
+		</div>
+		
+		<div class="container">
+			<div class="row">
+			
+			</div>
+			<div class="row">
+				<div class="col-md-2 "> </div>
+				<div class="col-md-10  page_header">
+								<h3><font color="#3C2A2A"><center>Daftar Anggota</center></font></h3>
+								<hr>
+				</div>	
+			</div>
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-10">
+				<table class="table table table table-responsive table-key-value pull-center">
+					<tbody>
+						<tr>
+							<th>No.</th>
+							<th>Nama</th>
+							<th>Email</th>
+							<th>Alamat</th>
+							<th>Action</th>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Amir</td>
+							<td>Amir@yahoo.com</td>
+							<td>Jl. Thamrin No.01</td>
+							<td><a href="#"><u>[edit]</u></a></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		
+</body>
+</html>
