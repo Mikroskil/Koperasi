c6f4b6285a42329e7dea744db1c48457068e98df
Ricky7 <black.id64@gmail.com>
2013-11-13 14:22:48 +0700

    Create AdminPage.php
    Halaman admin

---
+++ AdminPage.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Halaman Admin</title>
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">	
+	<link rel="stylesheet" href="fontawsome/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/a.css">
+		
+	<SCRIPT TYPE="text/javascript" src="js/jquery.js"></SCRIPT>
+ 
+ 	
+</head>
+<body>
+
+ 
+		<div class="sidebar">
+			<div class="heading1">
+			<img src="img/kope.png" class="logo">
+			<h1>Admin</h1>
+			<div class="grs"></div>
+			</div>
+			<nav>
+			<ul class="icons-ul">
+				<li><a href="add_admin.php"><i class="icon-plus-sign icon-li"></i>Tambah Admin</a><hr></li>
+				<li><a href="add_member.php"><i class="icon-plus-sign icon-li"></i>Tambah Anggota</a><hr></li>
+				<li><a href="#"><i class="icon-book icon-li"></i>Daftar Anggota</a><hr></li>
+				<li><a href="#"><i class="icon-off icon-li"></i>Keluar</a><hr></li>
+			</ul>
+			</nav>
+		</div>
+		
+</body>
+</html>
