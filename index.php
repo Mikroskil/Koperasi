e84a40e819f8ecd893659a0dcf7f697337224d64
Ricky7 <black.id64@gmail.com>
2013-11-13 14:24:24 +0700

    Create index.php
    halaman utama

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Merpati Pos</title>
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">	
+	<link rel="stylesheet" href="fontawsome/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/a.css">
+		
+	<SCRIPT TYPE="text/javascript" src="js/jquery.js"></SCRIPT>
+ 
+ 	
+</head>
+<body>
+
+ 
+		<div class="sidebar">
+			<div class="heading1">
+			<img src="img/kope.png" class="logo">
+			<h1>Merpati Pos</h1>
+			<div class="grs"></div>
+			</div>
+			<nav>
+			<ul class="icons-ul">
+				<li><a href="#"><i class="icon-home icon-li"></i>BERANDA</a><hr></li>
+				<li><a href="works.php"><i class="icon-book icon-li"></i>PROFIL PERUSAHAAN</a><hr></li>
+				<li><a href="regis.php"><i class="icon-plus-sign icon-li"></i>REGISTER</a><hr></li>
+			</ul>
+			</nav>
+		</div>
+		
+		
+		<div class="container">
+			
+				<div class="row">
+					
+						<div class=""> 
+							<div class="form-login-container">
+							
+							<form class="navbar-form pull-right">
+								<form id="form-login" action="#" method="post">
+									<fieldset>
+										<input type="text" id="username" name="username" placeholder="username" />
+										<input type="password" id="password" name="password" placeholder="password"/>
+										<button type="submit" class="btn" >Login</button>
+										<br><a href="#">Lupa Password?</a>
+									</fieldset>
+								</form>
+							</form>
+							
+							</div>
+						</div>
+						
+								 
+
+				</div>
+
+				<div class="row">
+					
+						<div class="col-md-2  "> </div>
+						<div class="col-md-10  page_header">
+								<h3><font color="#3C2A2A"><center>Sistem informasi simpan pinjam berbasis WEB pada koperasi simpan pinjam Merpati Pos</center></font></h3>
+								<hr>
+						</div>				 
+
+				</div>
+
+				<div class="row">
+					
+						<div class="col-md-3 "> </div>
+						<div class="col-md-9  konten">
+								
+
+								<div class="sosmed">
+									
+								</div>
+						</div>				 
+
+				</div>
+				
+
+		</div>
+
+
+ 
+	
+</body>
+</html>
