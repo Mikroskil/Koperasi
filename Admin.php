5768d687de8f5b2f7fe5bb06871605db3b659193
Ricky7 <black.id64@gmail.com>
2013-11-13 14:21:32 +0700

    Create Admin.php

---
+++ Admin.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Admin</title>
+
+</head>
+<body>
+	<div class="form-login-container">
+							
+							<form class="navbar-form pull-center">
+								<form id="form-login" action="#" method="post">
+									
+									<table class="table" width="300">
+										<tr><td>Username</td><td>: <input type="text" id="username" name="username" placeholder="username" /></td></tr>
+										<tr><td>Password</td><td>: <input type="password" id="password" name="password" placeholder="password"/></td></tr>
+										<tr><td></td><td><button type="submit" class="btn" >Login</button></td></tr>
+									</table>
+									
+								</form>
+							</form>
+							
+	</div>
+</body>
+</html>
