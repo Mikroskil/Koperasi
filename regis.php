5805367645fe2a81f03ea6f6b20c1242fd366556
Ricky7 <black.id64@gmail.com>
2013-11-13 14:26:32 +0700

    Create regis.php
    form pendaftaran oleh anggota

---
+++ regis.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Merpati Pos</title>
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">	
+	<link rel="stylesheet" href="fontawsome/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/a.css">
+		
+	<SCRIPT TYPE="text/javascript" src="js/jquery.js"></SCRIPT>
+ 
+ 	
+</head>
+<body>
+
+ 
+		<div class="sidebar">
+			<div class="heading1">
+			<img src="img/kope.png" class="logo">
+			<h1>Merpati Pos</h1>
+			<div class="grs"></div>
+			</div>
+			<nav>
+			<ul class="icons-ul">
+				<li><a href="index.php"><i class="icon-home icon-li"></i>BERANDA</a><hr></li>
+				<li><a href="works.php"><i class="icon-book icon-li"></i>PROFIL PERUSAHAAN</a><hr></li>
+				<li><a href="#"><i class="icon-plus-sign icon-li"></i>REGISTER</a><hr></li>
+			</ul>
+			</nav>
+		</div>
+		
+		
+		<div class="container">
+			
+				<div class="row">
+					
+						<div class=""> 
+							<div class="form-login-container fc">
+							
+							<form class="navbar-form pull-right">
+								<form id="form-login" action="#" method="post">
+									<fieldset>
+										<input type="text" id="username" name="username" placeholder="username" />
+										<input type="password" id="password" name="password" placeholder="password"/>
+										<button type="submit" class="btn" >Login</button>
+										<br><a href="#">Lupa Password?</a>
+									</fieldset>
+								</form>
+							</form>
+							
+							</div>
+						</div>
+						
+								 
+
+				</div>
+				
+				<div class="row">
+					
+						<div class="col-md-2  "> </div>
+						<div class="col-md-10  page_header">
+								<h3><font color="#3C2A2A"><center>Form Pendaftaran Anggota</center></font></h3>
+								<hr>
+						</div>				 
+
+				</div>
+				
+				<div class="row">
+					<div class="col-md-2"></div>
+					<div class="col-md-10">
+					
+						<font face="verdana,arial" size=-1>
+						<center>
+							
+														<table class="table table-responsive table-key-value pull-center">
+														<tbody>
+															<tr><td><font face="verdana,arial" size=-1><center>Name</center> </font></td><td>:<input type="text" name="name" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Address</center> </font></td><td>:<textarea cols="18" name="ad"></textarea></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Gender</center></font></td>
+															<td>
+																<form>
+																	:<input type="radio" name="sex" value="male">Male
+																	<input type="radio" name="sex" value="female">Female
+																</form>
+															</td>
+															</tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Username</center></font></td><td>:<input type="text" name="user" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Password</center></font></td><td>:<input type="password" name="password" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Confirm Password</center></font></td><td>:<input type="text" name="confirm" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Activation Code</center></font></td><td>:<input type="text" name="kode" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Email</center></font></td><td>:<input type="text" name="email" /></td></tr>
+															<tr><td><font face="verdana,arial" size=-1><center>Contact Person </font></td><td>:<input type="text" name="cp" /></td></tr>
+															<tr><td></td><td><button type="submit" class="btn"><center>Register</center></button></td></tr>
+															
+														</tbody>
+														</table>
+							
+					</div>
+				</div>
+		</div>
+
+
+		
+
+
+ 
+	
+</body>
+</html>
