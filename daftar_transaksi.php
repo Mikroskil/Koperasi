920b5990503bb127fe1cda8652db448fc342c457
Ricky7 <black.id64@gmail.com>
2013-11-13 14:23:31 +0700

    Create daftar_transaksi.php

---
+++ daftar_transaksi.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Merpati Pos</title>
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">	
+	<link rel="stylesheet" href="fontawsome/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/a.css">
+
+
+       
+		
+	<SCRIPT TYPE="text/javascript" src="js/jquery.js"></SCRIPT>
+ 
+ 	
+</head>
+<body>
+
+ 
+		<div class="sidebar">
+			<div class="heading1">
+			<img src="img/kope.png" class="logo">
+			<h1>Merpati Pos</h1>
+			<div class="grs"></div>
+			</div>
+			<nav>
+			<ul class="icons-ul">
+				<li><a href="pinjam.php"><i class="icon-plus-sign icon-li"></i>AJUKAN PINJAMAN</a><hr></li>
+				<li><a href="#"><i class="icon-user icon-li"></i>LIHAT DAFTAR TRANSAKSI</a><hr></li>
+				<li><a href="index.php"><i class="icon-off icon-li"></i>KELUAR</a><hr></li>
+			</ul>
+			</nav>
+		</div>
+		
+		
+		<div class="container">
+			
+				<div class="row">
+					
+						<div class="pull-right">
+							<h5>Berhasil Masuk <i class="icon-thumbs-up"></i></h5>
+						</div>
+						
+								 
+
+				</div>
+				
+				<div class="row">
+					
+						<div class="col-md-2  "> </div>
+						<div class="col-md-10  page_header">
+								<h3><font color="#3C2A2A"><center>Daftar Transaksi</center></font></h3>
+								<hr>
+						</div>				 
+
+				</div>
+				
+				<div class="row">
+					<div class="col-md-2"></div>
+					<div class="col-md-6">
+					<form>
+						<table class="table table-responsive table-key-value table-striped pull-left">
+						<tbody>
+							<tr>
+								<td>NIK	</td>
+								<td>:</td>
+								<td>0123456</td>
+							</tr>
+							<tr>
+								<td>Nama</td>
+								<td>:</td>
+								<td>Amir</td>
+							</tr>
+							<tr>
+								<td>Email</td>
+								<td>:</td>
+								<td>Amir@yahoo.com</td>
+							</tr>
+							<tr>
+								<td>No. Telp</td>
+								<td>:</td>
+								<td>081234567890</td>
+							</tr>
+							<tr>
+								<td>Status</td>
+								<td>:</td>
+								<td><i>Belum Pinjam</i></td>
+							</tr>
+						</tbody>
+						<table>
+					</form>
+					</div>
+					<div class="col-md-4"></div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-2"></div>
+					<div class="col-md-10">
+						<form>
+							<table class="table table-responsive table-key-value pull-center">
+								<tbody>
+									<tr>
+										<th>NO.</th>
+										<th>Tanggal</th>
+										<th>Nominal Peminjaman</th>
+										<th>Sisa Hutang</th>
+										<th>Status</th>
+									</tr>
+									<tr>
+										<td>1</td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>2</td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>3</td>
+										<td></td>
+										<td></td>
+										<td></td>
+										<td></td>
+									</tr>
+								</tbody>
+							<table>
+						</form>
+					</div>
+				</div>
+		</div>
+
+
+		
+
+
+ 
+	
+</body>
+</html>
